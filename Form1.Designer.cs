517d309aa9608119820772185c403a445c281940
Mahdi Adib <mahdiadib2001@gmail.com>
2023-02-14 22:49:14 +0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace FixTheCat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReshuffle = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox00 = new System.Windows.Forms.PictureBox();
+            this.pictureBox02 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox01 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            this.pictureBox20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.lblClicks = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReshuffle
+            // 
+            this.btnReshuffle.Location = new System.Drawing.Point(612, 117);
+            this.btnReshuffle.Name = "btnReshuffle";
+            this.btnReshuffle.Size = new System.Drawing.Size(95, 41);
+            this.btnReshuffle.TabIndex = 10;
+            this.btnReshuffle.Text = "Reshuffle";
+            this.btnReshuffle.UseVisualStyleBackColor = true;
+            this.btnReshuffle.Click += new System.EventHandler(this.btnReshuffle_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FixTheCat.Properties.Resources.cat1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox00
+            // 
+            this.pictureBox00.Location = new System.Drawing.Point(281, 117);
+            this.pictureBox00.Name = "pictureBox00";
+            this.pictureBox00.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox00.TabIndex = 1;
+            this.pictureBox00.TabStop = false;
+            this.pictureBox00.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox00.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox00.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox02
+            // 
+            this.pictureBox02.Location = new System.Drawing.Point(493, 117);
+            this.pictureBox02.Name = "pictureBox02";
+            this.pictureBox02.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox02.TabIndex = 2;
+            this.pictureBox02.TabStop = false;
+            this.pictureBox02.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox02.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox02.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(281, 223);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 3;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox10.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox10.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox01
+            // 
+            this.pictureBox01.Location = new System.Drawing.Point(387, 117);
+            this.pictureBox01.Name = "pictureBox01";
+            this.pictureBox01.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox01.TabIndex = 4;
+            this.pictureBox01.TabStop = false;
+            this.pictureBox01.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox01.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox01.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(387, 223);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 5;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox11.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox11.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.Location = new System.Drawing.Point(493, 328);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox22.TabIndex = 6;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox22.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox22.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox20
+            // 
+            this.pictureBox20.Location = new System.Drawing.Point(281, 328);
+            this.pictureBox20.Name = "pictureBox20";
+            this.pictureBox20.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox20.TabIndex = 7;
+            this.pictureBox20.TabStop = false;
+            this.pictureBox20.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox20.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox20.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.Location = new System.Drawing.Point(387, 328);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox21.TabIndex = 8;
+            this.pictureBox21.TabStop = false;
+            this.pictureBox21.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox21.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox21.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Location = new System.Drawing.Point(493, 223);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(105, 105);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 9;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.OnPicBoxClick);
+            this.pictureBox12.MouseEnter += new System.EventHandler(this.OnPicBoxMouseEnter);
+            this.pictureBox12.MouseLeave += new System.EventHandler(this.OnPicBoxMouseLeave);
+            // 
+            // lblClicks
+            // 
+            this.lblClicks.AutoSize = true;
+            this.lblClicks.Location = new System.Drawing.Point(281, 480);
+            this.lblClicks.Name = "lblClicks";
+            this.lblClicks.Size = new System.Drawing.Size(47, 13);
+            this.lblClicks.TabIndex = 12;
+            this.lblClicks.Text = "Clicks: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 582);
+            this.Controls.Add(this.lblClicks);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnReshuffle);
+            this.Controls.Add(this.pictureBox00);
+            this.Controls.Add(this.pictureBox02);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox01);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox20);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox12);
+            this.Name = "Form1";
+            this.Text = "Form 1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox00;
+        private System.Windows.Forms.PictureBox pictureBox02;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox01;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox22;
+        private System.Windows.Forms.PictureBox pictureBox20;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Button btnReshuffle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblClicks;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixTheCat
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[,] picBoxes = new PictureBox[3, 3];
+        Bitmap[,] images = new Bitmap[3, 3];
+        string[,] tags = new string[3, 3];
+        int clicks = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillForm();
+            Shuffle();
+        }
+
+
+        void FillForm()
+        {
+            picBoxes[0, 0] = pictureBox00;
+            picBoxes[0, 1] = pictureBox01;
+            picBoxes[0, 2] = pictureBox02;
+            picBoxes[1, 0] = pictureBox10;
+            picBoxes[1, 1] = pictureBox11;
+            picBoxes[1, 2] = pictureBox12;
+            picBoxes[2, 0] = pictureBox20;
+            picBoxes[2, 1] = pictureBox21;
+            picBoxes[2, 2] = pictureBox22;
+
+            images[0, 0] = Properties.Resources.solid_color_image;
+            images[0, 1] = Properties.Resources.c2;
+            images[0, 2] = Properties.Resources.c3;
+            images[1, 0] = Properties.Resources.c4;
+            images[1, 1] = Properties.Resources.c5;
+            images[1, 2] = Properties.Resources.c6;
+            images[2, 0] = Properties.Resources.c7;
+            images[2, 1] = Properties.Resources.c8;
+            images[2, 2] = Properties.Resources.c9;
+
+            tags[0, 0] = "T00";
+            tags[0, 1] = "T01";
+            tags[0, 2] = "T02";
+            tags[1, 0] = "T10";
+            tags[1, 1] = "T11";
+            tags[1, 2] = "T12";
+            tags[2, 0] = "T20";
+            tags[2, 1] = "T21";
+            tags[2, 2] = "T22";
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    picBoxes[i, j].Image = images[i, j];
+                    picBoxes[i, j].Tag = tags[i, j];
+                }
+            }
+        }
+
+
+        bool CheckWinner()
+        {
+            //MessageBox.Show("Winner checking");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (tags[i, j] != picBoxes[i, j].Tag.ToString()) return false;
+                }
+            }
+            return true;
+        }
+
+
+        void Shuffle()
+        {
+            SwapPicBoxImage(ref pictureBox00, ref pictureBox01);
+            SwapPicBoxImage(ref pictureBox01, ref pictureBox11);
+            SwapPicBoxImage(ref pictureBox11, ref pictureBox21);
+
+
+            //Random rnd = new Random();
+            //int swaps = rnd.Next(10, 21);
+            ////MessageBox.Show($"{swaps}");
+
+            //for (int i = 1; i <= swaps; i++)
+            //{
+            //    int row1 = rnd.Next(0, 3);
+            //    int row2 = rnd.Next(0, 3);
+            //    int col1 = rnd.Next(0, 3);
+            //    int col2 = rnd.Next(0, 3);
+            //    //MessageBox.Show($"{row1}, {col1}, {row2}, {col2}");
+            //    SwapPicBoxImage(ref picBoxes[row1, col1], ref picBoxes[row2, col2]);
+            //}
+
+            clicks = 0;
+            lblClicks.Text = $"Clicks: {clicks}";
+        }
+
+
+        void SwapPicBoxImage(ref PictureBox a, ref PictureBox b)
+        {
+            Image tmp; string stmp;
+
+            tmp = a.Image; stmp = a.Tag.ToString();
+            a.Image = b.Image; a.Tag = b.Tag;
+            b.Image = tmp; b.Tag = stmp;
+        }
+
+
+        bool isValid(int i, int j)
+        {
+            //MessageBox.Show($"{i}, {j}: {i >= 0 && j >= 0 && i < 3 && j < 3}");
+            return i >= 0 && j >= 0 && i < 3 && j < 3;
+        }
+
+
+        void GoodClickWorks(int x, int y)
+        {
+            picBoxes[x, y].Size = new Size(105, 105);
+            lblClicks.Text = $"Clicks: {++clicks}";
+            bool win = CheckWinner();
+            if (win)
+            {
+                DialogResult dr = MessageBox.Show($"Congratulations! You fixed the cat in {clicks} clicks!\nDo you wish to play again?", "Winner", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes) Shuffle();
+                else Application.Exit();
+            }
+        }
+
+
+        private void OnPicBoxClick(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            int x = -1, y = -1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (picBoxes[i, j].Image == p.Image)
+                    {
+                        x = i;
+                        y = j;
+                        break;
+                    }
+                }
+            }
+
+
+            if (isValid(x + 1, y))
+            {
+                var b1 = Properties.Resources.solid_color_image;
+                var b2 = (Bitmap)picBoxes[x + 1, y].Image;
+                var res = ComparingImages.Compare(b1, b2);
+
+                if (res == ComparingImages.CompareResult.ciCompareOk)
+                {
+                    SwapPicBoxImage(ref picBoxes[x, y], ref picBoxes[x + 1, y]);
+                    GoodClickWorks(x, y);
+                    return;
+                }
+            }
+            
+            if (isValid(x - 1, y))
+            {
+                var b1 = Properties.Resources.solid_color_image;
+                var b2 = (Bitmap)picBoxes[x - 1, y].Image;
+                var res = ComparingImages.Compare(b1, b2);
+
+                if (res == ComparingImages.CompareResult.ciCompareOk)
+                {
+                    SwapPicBoxImage(ref picBoxes[x, y], ref picBoxes[x - 1, y]);
+                    GoodClickWorks(x, y);
+                    return;
+                }
+            }
+            
+            if (isValid(x, y + 1))
+            {
+                var b1 = Properties.Resources.solid_color_image;
+                var b2 = (Bitmap)picBoxes[x, y + 1].Image;
+                var res = ComparingImages.Compare(b1, b2);
+
+                if (res == ComparingImages.CompareResult.ciCompareOk)
+                {
+                    SwapPicBoxImage(ref picBoxes[x, y], ref picBoxes[x, y + 1]);
+                    GoodClickWorks(x, y);
+                    return;
+                }
+            }
+            
+            if (isValid(x, y - 1))
+            {
+                var b1 = Properties.Resources.solid_color_image;
+                var b2 = (Bitmap)picBoxes[x, y - 1].Image;
+                var res = ComparingImages.Compare(b1, b2);
+
+                if (res == ComparingImages.CompareResult.ciCompareOk)
+                {
+                    SwapPicBoxImage(ref picBoxes[x, y], ref picBoxes[x, y - 1]);
+                    GoodClickWorks(x, y);
+                    return;
+                }
+            }
+
+            //if (!moveOver) MessageBox.Show("No space to move.");
+        }
+
+        private void OnPicBoxMouseEnter(object sender, EventArgs e)
+        {
+            var b1 = Properties.Resources.solid_color_image;
+            var b2 = (Bitmap)((PictureBox)sender).Image;
+            var res = ComparingImages.Compare(b1, b2);
+            if (res != ComparingImages.CompareResult.ciCompareOk) ((PictureBox)sender).Size = new Size(110, 110);
+        }
+
+        private void OnPicBoxMouseLeave(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).Size = new Size(105, 105);
+        }
+
+        private void btnReshuffle_Click(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+    }
+
+
+
+
+    public class ComparingImages
+    {
+        public enum CompareResult
+        {
+            ciCompareOk,
+            ciPixelMismatch,
+            ciSizeMismatch
+        };
+
+        public static CompareResult Compare(Bitmap bmp1, Bitmap bmp2)
+        {
+            CompareResult cr = CompareResult.ciCompareOk;
+
+            //Test to see if we have the same size of image
+            if (bmp1.Size != bmp2.Size)
+            {
+                cr = CompareResult.ciSizeMismatch;
+            }
+            else
+            {
+                //Convert each image to a byte array
+                System.Drawing.ImageConverter ic = new System.Drawing.ImageConverter();
+                byte[] btImage1 = new byte[1];
+                btImage1 = (byte[])ic.ConvertTo(bmp1, btImage1.GetType());
+                byte[] btImage2 = new byte[1];
+                btImage2 = (byte[])ic.ConvertTo(bmp2, btImage2.GetType());
+
+                //Compute a hash for each image
+                SHA256Managed shaM = new SHA256Managed();
+                byte[] hash1 = shaM.ComputeHash(btImage1);
+                byte[] hash2 = shaM.ComputeHash(btImage2);
+
+                //Compare the hash values
+                for (int i = 0; i < hash1.Length && i < hash2.Length && cr == CompareResult.ciCompareOk; i++)
+                {
+                    if (hash1[i] != hash2[i]) cr = CompareResult.ciPixelMismatch;
+                }
+            }
+            return cr;
+        }
+    }
+
+}
